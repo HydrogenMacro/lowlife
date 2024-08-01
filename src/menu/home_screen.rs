08b810febb5c02d12ca9273bd10da9e3ed6ec854
hydrogenmacro <hydrogenmacro@gmail.com>
2024-08-01 17:39:01 -0400

    yakui

--- src/menu/home_screen.rs
+++ src/menu/home_screen.rs
@@ -1,16 +1,23 @@
+use macroquad::hash;
+use macroquad::miniquad::window::screen_size;
 use macroquad::prelude::*;
+use macroquad::ui::root_ui;
 use crate::scene::Scene;
 
 #[derive(Default)]
 pub struct HomeScreen {
-
+	a: u32
 }
 impl Scene for HomeScreen {
 	fn update(&mut self) {
 
 	}
 
-	fn draw(&self) {
-		draw_circle(0., 0., 10., RED);
+	fn draw(&mut self) {
+		yakui_macroquad::ui(|yakui| {
+			yakui::center(|| {
+				yakui::text(50., "hi");
+			});
+		});
 	}
 }
