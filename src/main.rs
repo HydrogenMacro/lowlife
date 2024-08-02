def81075f50af34820f7b7b3c050b7b0d476677d
hydrogenmacro <hydrogenmacro@gmail.com>
2024-08-01 20:04:46 -0400

    bevy

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,14 @@
-#![feature(impl_trait_in_assoc_type)]
-mod menu;
-mod game;
-mod scene;
+mod home_screen;
 
-use std::thread;
-use macroquad::prelude::*;
-use macroquad::experimental::collections::storage;
-use crate::menu::HomeScreen;
-use crate::scene::{CurrentScene, Scene};
+use bevy::math::vec2;
+use bevy::prelude::*;
 
-#[macroquad::main("Lowlife")]
-async fn main() {
-	CurrentScene::init(HomeScreen::default());
-
-	loop {
-		clear_background(DARKGREEN);
-		storage::get_mut::<CurrentScene>().update();
-		storage::get_mut::<CurrentScene>().draw();
-		yakui_macroquad::draw();
-		next_frame().await;
-	}
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+	    .add_systems(Update, a)
+        .run();
+}
+fn a(mut gizmos: Gizmos,) {
+	gizmos.line_2d(vec2(-10., -10.), vec2(10., 10.), Color::srgb(1., 1., 0.));
 }
--- src/menu/mod.rs
+++
@@ -1,2 +0,0 @@
-mod home_screen;
-pub use home_screen::*;
